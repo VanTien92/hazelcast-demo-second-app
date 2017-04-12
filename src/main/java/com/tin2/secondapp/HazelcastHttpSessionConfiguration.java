79974b12574cf1694e1ec81748c33854c2dbc160
Tien Ngo <tien.ngo@axonactive.vn>
2017-04-12 10:57:25 +0700

    Change port

--- src/main/java/com/tin2/secondapp/HazelcastHttpSessionConfiguration.java
+++ src/main/java/com/tin2/secondapp/HazelcastHttpSessionConfiguration.java
@@ -42,7 +42,6 @@ public class HazelcastHttpSessionConfiguration {
                 new MapIndexConfig( HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE, false ) );
 
         config.getNetworkConfig().setPublicAddress( publicAddress );
-        config.getNetworkConfig().setPortAutoIncrement(false);
         config.getNetworkConfig().setPort( port );
         config.getNetworkConfig().setPortCount( portCount );
 
